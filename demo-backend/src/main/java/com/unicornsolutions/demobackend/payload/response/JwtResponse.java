5151b0e0831ea844ac70712f099d35d3d40ddf34
meraihan <sayedmahmudraihan@gmail.com>
2020-11-26 02:21:52 +0600

    backend updates

--- demo-backend/src/main/java/com/unicornsolutions/demobackend/payload/response/JwtResponse.java
+++ demo-backend/src/main/java/com/unicornsolutions/demobackend/payload/response/JwtResponse.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import java.util.List;
 
-@Data
 public class JwtResponse {
 
     private String token;
@@ -20,5 +19,49 @@ public class JwtResponse {
         this.username = username;
         this.email = email;
         this.roles = roles;
+    }
+
+    public String getAccessToken() {
+        return token;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.token = accessToken;
+    }
+
+    public String getTokenType() {
+        return type;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.type = tokenType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
     }
 }
--- demo-backend/src/main/java/com/unicornsolutions/demobackend/repository/UserRepository.java
+++ demo-backend/src/main/java/com/unicornsolutions/demobackend/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.unicornsolutions.demobackend.repository;
 
 import com.unicornsolutions.demobackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
