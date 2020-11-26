cd67f61f0a21d19e40d9ddda62355a0904130a4b
meraihan <sayedmahmudraihan@gmail.com>
2020-11-26 15:42:16 +0600

    client updates

--- demo-client/src/app/app.component.ts
+++ demo-client/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo-client';
+export class AppComponent implements OnInit {
+  private roles: string[];
+  isLoggedIn = false;
+  showAdmin = false;
+  showUser = false;
+  username: string;
+
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit() {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdmin = this.roles.includes('ROLE_ADMIN');
+      this.showUser = this.roles.includes('ROLE_USER');
+
+      this.username = user.username;
+    }
+  }
+
+  logout() {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
