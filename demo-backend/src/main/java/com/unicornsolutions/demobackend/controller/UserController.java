8385c280d52cd5077da4b3791322b98385c12fbb
meraihan <sayedmahmudraihan@gmail.com>
2020-11-30 10:51:35 +0600

    CRUD API

--- demo-backend/src/main/java/com/unicornsolutions/demobackend/controller/UserController.java
+++ demo-backend/src/main/java/com/unicornsolutions/demobackend/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.unicornsolutions.demobackend.controller;
 
+import com.unicornsolutions.demobackend.exception.ResourceNotFoundException;
+import com.unicornsolutions.demobackend.model.User;
+import com.unicornsolutions.demobackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/test")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/all")
     public String allAccess() {
@@ -26,5 +36,49 @@ public class UserController {
     @PreAuthorize("hasRole('ADMIN')")
     public String adminAccess() {
         return "Admin Board.";
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long id)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + id));
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@Valid @RequestBody User user) {
+        return userRepository.saveAndFlush(user);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long id,
+                                                               @Valid @RequestBody User user) throws ResourceNotFoundException {
+        User findUser = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + id));
+
+        findUser.setId(user.getId());
+        findUser.setUsername(user.getUsername());
+        findUser.setEmail(user.getEmail());
+        findUser.setRoles(user.getRoles());
+        final User updatedUser = userRepository.saveAndFlush(findUser);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Map<String, Boolean> deleteUsers(@PathVariable(value = "id") Long id)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + id));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
